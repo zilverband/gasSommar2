71f72920904d83fcb93210bb7b8f0201cfa7258b
William Olsson <william@sensorbee.com>
2025-06-25 11:36:02 +0200

    Added function for converting all eleents in a dataframe

--- unit_conversion.py
+++ unit_conversion.py
@@ -1,3 +1,13 @@
+from functools import partial
+import math
+def df_ugm3_to_ppb(data, units, gas, temperature_c=25):
+
+    
+    g = partial(ugm3_to_ppb, gas = gas,temperature_c=temperature_c)
+    data[gas] = data[gas].apply(g)
+    units[gas] = "(ppb)"
+    return data,units
+
 def ugm3_to_ppb(ug_per_m3, gas, temperature_c=25):
     """
     Converts Gas concentration from ug_per_m3 to ppb
@@ -10,6 +20,8 @@ def ugm3_to_ppb(ug_per_m3, gas, temperature_c=25):
     Returns:
         Gas concentration in ppb
     """
+    if ug_per_m3 is None:
+        return ug_per_m3
 
     #https://www.engineeringtoolbox.com/molecular-weight-gas-vapor-d_1156.html
     mol_weights = {
@@ -19,9 +31,8 @@ def ugm3_to_ppb(ug_per_m3, gas, temperature_c=25):
         'CO' : 28.011,
         'CO2' : 44.01
     }
-
     M = mol_weights[gas]
     temperature_k = temperature_c + 273.15
 
-    ppb = temperature_k*ug_per_m3 / (12.187*M)
+    ppb = temperature_k * ug_per_m3 / (12.187*M)
     return ppb
