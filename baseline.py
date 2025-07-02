4ef1523af06eab30c539e35b2fcf4e079b5c9c7c
William Olsson <william@sensorbee.com>
2025-07-02 15:51:40 +0200

    Changed some names and functionality to make it simpler. Also added documentation

--- baseline.py
+++ baseline.py
@@ -1,66 +1,70 @@
-import load_data as ld
 import pandas as pd
-from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import numpy as np
-from unit_conversion import df_ugm3_to_ppb
 
-def lowest_points(data,ratio,tresh,plot=True,return_points=False):
+def lowest_points(data, curr, delta, period = None, ratio=1/20, tresh=0,
+                   plot=False, return_points=False,filters=["hampel","split"]):
+    """Returns a baseline estimate based on the lowest points
+
+    Parameters:
+    data : Series - containing sensor data
+    curr : datetime - current time
+    delta : timedelta - how far back we want to look
+    period : timedelta - if not None takes the lowest point in each period
+    ratio : ratio of points to be considered low
+    tresh : if non-zero removes the lowest points (to omit outliers)
+    plot : True,false if we should plot the baseline and points
+    return_points : if we should return the points (not y,R)
+    """
+    data = data[(curr-delta):curr]
+    #Exits code if there is no data (otherwise it will crash)
     if data.empty:
-        return None
+        return np.nan, np.nan
     
-    N = len(data)
-    num_of_base_points = int(N*ratio)
-    outliers = int(num_of_base_points*tresh)
+    if period is None:
+        period = delta
     
-    base_points = data.nsmallest(num_of_base_points)[outliers:]
-
-    if plot:
-        plt.scatter(base_points.index,base_points,color="blue",zorder=5)
-        plt.hlines(y=base_points.mean(),xmin=data.index[0],xmax=data.index[-1],color="blue")
-
-    if return_points:
-        return base_points
-    
-    y = base_points.mean()
-    R = base_points.var()
-    return y,R
-
-def lowest_points_interval(data,delta,ratio,tresh,plot=True,return_points=False):
-    if data.empty:
-        return None
-    
+    #Calculates base point for each interval
     start = data.index[0]
     end = data.index[-1]
     base_points=None
-    
+
     while start < end:
-        new_points = lowest_points(data[start:(start+delta)],ratio,tresh,plot=False,return_points=True)
+
+        curr_data = data[start:(start+period)]
+        N = len(curr_data)
+        num_of_base_points = int(N*ratio)
+        outliers = int(num_of_base_points*tresh)
+        new_points = curr_data.nsmallest(num_of_base_points)[outliers:]
+        
         if base_points is None:
             base_points= new_points
         else:
             base_points = pd.concat([base_points,new_points])
-        start += delta
+        start += period
 
-    base_points = hampel_filter(base_points)
-    base_points = split_in_two(base_points)
-    
-    if plot:
-        plt.scatter(base_points.index,base_points,color="blue",zorder=5)
-        plt.hlines(y=base_points.mean(),xmin=data.index[0],xmax=end,color="blue")
-
+    #Does filtering on the base points
+    if "hampel" in filters:
+        base_points = hampel_filter(base_points)
+    if "split" in filters:
+        base_points = split_in_two(base_points)
     if return_points:
         return base_points
-    
 
+    if plot:
+        plt.scatter(base_points.index,base_points,color="blue",zorder=5)
+        plt.hlines(y=base_points.mean(),xmin=data.index[0],
+                   xmax=end,color="blue")
+
+    #Calculates mean and variance of base points
     y = base_points.mean()
-    R= base_points.var()
+    R = base_points.var()
     return y,R
 
 def hampel_filter(data,n_sigmas=2):
+    #Simple hampel filter
     mu = data.mean()
     sigma = data.std()
-
     return data[np.abs(data-mu) < n_sigmas*sigma]
 
 def split_in_two(data,tresh=10):
@@ -69,6 +73,9 @@ def split_in_two(data,tresh=10):
     data1 = data[data<mean]
     data2 = data[data>mean]
     test = np.abs(data1.mean()-data2.mean())
+
+    # if we can split it into two parts that are more than tresh ppb 
+    # away from each other, use only the last part
     if test > tresh:
         if data1.index.mean() > data2.index.mean():
             return data1
--- kalman.py
+++ kalman.py
@@ -1,58 +1,91 @@
+"""Contains the class KalmanCalibrator, used to do a baseline calibration of sensor data"""
+from datetime import timedelta
 import pandas as pd
-from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import numpy as np
 
 #sensor containing only the gas value of interest!
 class KalmanCalibrator:
-    def __init__(self,Q,m_func,sensor,delta=timedelta(days=7),look_back=None,x0=0,P0=1, backwards=False):
-        self.Q = Q* (delta.days ** 3) #Number
+    """ Contains a Kalman filter for tracking the baseline from a sensor.
+
+    The time update step assumes the sensor is constant, with variance Q
+    The measurement update step is provided with m_func
+
+    Attributes:
+    Q : number, the variance of the baseline per day (model parameter)
+    x0 : initial value
+    P0 : initial variance
+    step : size of time step (type timedelta)
+    backwards : False if we calibrate forwards True if backwards
+    baseline : expected baseline
+
+    Methods:
+    time_update() : performs a time update step
+    measure_update(): performs a measurement update step
+    run(end=None,verbal=False) : runs the filter to point end 
+                        (end of data if end=None), prints updates if verbal=True
+    return_calibrated_series : returns calibrated data as a pandas Series
+
+    Example: runs the calibrator and plots data
+    measure - some function, takes a pandas series with sensor data and a time as argument
+    sensor - pandas series with sensor data
+
+    calibrator = KalmanCalibrator(measure, sensor)
+    calibrator.run()
+    calibrator.plot()
+    plt.show()
+    """
+
+    def __init__(self,m_func, sensor ,Q=1, x0=0, P0=1, step=timedelta(days=1),
+                  backwards=False, baseline = 0):
+        #Defines all Kalman filter variables
+        self.Q = Q*step.total_seconds()/86400 #Scaling the time variance with time
         self.m_func = m_func #function that returns y and R
-        self.x = x0 #Number
-        self.P = P0 #Number
-        self.sensor = sensor #pd.Series
-        self.delta = delta #timedelta
-        self.backwards = backwards #Bool
-        self.t = self.sensor.index[0].round("D") #datetime
-        self.offset = pd.DataFrame(index=sensor.index, columns=["offset","variance"])
+        self.x = x0
+        self.P = P0
+        self.t = sensor.index[0].round("D")
+        self.step = step
         
-        if look_back is None:
-            self.look_back = delta
-        else:
-            self.look_back = look_back
+        self.sensor = sensor #saves the sensor data
+
+        self.backwards = backwards
+        self.offset = pd.DataFrame(index=sensor.index, columns=["offset","variance"])
+        self.baseline = baseline
 
     def time_update(self):
+        #Kalman formula
         self.x = self.x
         self.P = self.P + self.Q
 
+        #Saves the offset
         if not self.backwards:
-            self.offset.loc[self.t:(self.t+self.delta),"offset"] = self.x
-            self.offset.loc[self.t:(self.t+self.delta),"variance"] = self.P
+            self.offset.loc[self.t:(self.t+self.step),"offset"] = self.x
+            self.offset.loc[self.t:(self.t+self.step),"variance"] = self.P
         
-        self.t = self.t + self.delta
+        self.t = self.t + self.step
 
-    def measurement_update(self):
-        #Check a week rolling average
-        curr_time = self.sensor[(self.t - self.look_back):self.t]
-        if curr_time.empty:
-            return None
-        y, R = self.m_func(curr_time)
+    def measurement_update(self):  
+        y, R = self.m_func(self.sensor,self.t)
         if np.isnan(y) or np.isnan(R):
             return None
 
+        #Kalman formula
         x = self.x
         P = self.P
         self.x = x + P/(P+R)*(y-x)
         self.P = P - P**2/(P+R)
-        
+
+        #Saves the offset
         if self.backwards:
-            self.offset.loc[(self.t - self.delta):self.t,"offset"] = self.x
-            self.offset.loc[(self.t - self.delta):self.t,"variance"] = self.P
+            self.offset.loc[(self.t - self.step):self.t,"offset"] = self.x
+            self.offset.loc[(self.t - self.step):self.t,"variance"] = self.P
     
     def run(self,end=None, verbal=False):
+        #If end isn't specified, run until we're out of data
         if end is None:
             end = self.sensor.index[-1]
         
+        #Update step
         while self.t < end:
             self.time_update()
             self.measurement_update()
@@ -60,28 +93,29 @@ class KalmanCalibrator:
                 self.print_state()
     
     def return_calibrated_series(self,truncate = True):
-        cal_data = self.sensor-self.offset["offset"]
+        cal_data = self.sensor-self.offset["offset"] + self.baseline
+        
         if truncate:
             cal_data[cal_data < 0] = 0
+        
         return cal_data
 
-    def plot_offset(self, confidence=True):
+    def plot_offset(self, confidence=True, color = "blue"):
         if confidence:
             std = np.sqrt(self.offset["variance"].astype("Float64"))
             ci = 1.96 * std
 
-            self.offset["offset"].plot(color="blue")
-            (self.offset["offset"]+ ci).plot(color="blue",style = "--")
-            (self.offset["offset"]-ci).plot(color="blue",style = "--")
+            self.offset["offset"].plot(color=color)
+            (self.offset["offset"]+ ci).plot(color=color,style = "--")
+            (self.offset["offset"]-ci).plot(color=color,style = "--")
         else:
-            self.offset["offset"].plot(color="blue")
+            self.offset["offset"].plot(color=color)
 
-    def plot(self):
-        self.sensor.plot()
-        self.return_calibrated_series().plot()
-        plt.show()
-        self.plot_offset()
-        plt.show()
+    def plot(self,color="green"):
+        self.return_calibrated_series().plot(color=color)
+
+    def plot_raw(self,color="yellow"):
+        self.sensor.plot(color=color)
 
     def print_state(self):
         print("x: {0} P: {1} t: {2}".format(self.x,self.P,self.t))
